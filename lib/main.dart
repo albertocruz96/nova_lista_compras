51a8c4b4c5510d96830f760a73ac7323f562bda6
origin <https://github.com/albertocruz96/AppMercado.git>
2021-11-19 16:28:11 -0300

    Revert "Start Commit"
    This reverts commit 4d33f35e

--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:novalistacompras/Home.dart';
-
-void main() async {
-  runApp(MaterialApp(
-    home: Home(),
-    debugShowCheckedModeBanner: false,
-  ));
-}
